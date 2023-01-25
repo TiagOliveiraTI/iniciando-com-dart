ad7165c290e17b91e58ed6f20f88cbb501d97aa6
Tiago Oliveira <contato@otao.dev.br>
2023-01-25 09:26:34 -0300

    chore: create initial structure

---
+++ bin/iniciando_com_dart.dart
@@ -0,0 +1,5 @@
+import 'package:iniciando_com_dart/iniciando_com_dart.dart' as iniciando_com_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${iniciando_com_dart.calculate()}!');
+}
---
+++ test/iniciando_com_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:iniciando_com_dart/iniciando_com_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
