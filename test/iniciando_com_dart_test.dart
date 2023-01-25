efb38c218a2d032834b9712963c316fef386f2b3
Tiago Oliveira <contato@otao.dev.br>
2023-01-25 14:14:45 -0300

    test: create a int value

--- test/iniciando_com_dart_test.dart
+++ test/iniciando_com_dart_test.dart
@@ -2,7 +2,7 @@ import 'package:iniciando_com_dart/iniciando_com_dart.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('should return a int value', () {
+    expect(calculateAge(DateTime(1988, 01, 31)), 35);
   });
 }
