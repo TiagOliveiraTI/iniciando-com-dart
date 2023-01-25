bd140acc9e44e81924654b49f222560ed0ef0871
Tiago Oliveira <contato@otao.dev.br>
2023-01-25 14:35:55 -0300

    test: create a double value

--- test/iniciando_com_dart_test.dart
+++ test/iniciando_com_dart_test.dart
@@ -6,5 +6,12 @@ void main() {
     expect(calculateAge(DateTime(1988, 01, 31)), 35);
   });
 
-  
+  test('should return a value of IMC', () {
+    double weight = 77.6;
+    double height = 1.74;
+
+    final imc = weight / (height * height);
+
+    expect(calculateImc(weight, height), 25.63086272955476);
+  });
 }
